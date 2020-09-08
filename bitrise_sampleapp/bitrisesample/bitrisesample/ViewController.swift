aacdce11293a1b3dbcc0a5d713ba904bb5d35a14
Quintin Rodriguez-Harrison <Quintin.Rodriguez-Harrison@wwt.com>
2020-09-08 13:51:41 -0500

    Initial project commit - Q

---
+++ bitrise_sampleapp/bitrisesample/bitrisesample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitrisesample
+//
+//  Created by Rodriguez-Harrison, Quintin on 9/8/20.
+//  Copyright © 2020 Rodriguez-Harrison, Quintin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
