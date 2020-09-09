aa3807085464f6c47e02cdd377599df14e9603ea
Quintin Rodriguez-Harrison <Quintin.Rodriguez-Harrison@wwt.com>
2020-09-09 10:29:00 -0500

    Pushed a fresh build of the project.

---
+++ bitrisesampleapp/bitrisesampleapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitrisesampleapp
+//
+//  Created by Rodriguez-Harrison, Quintin on 9/9/20.
+//  Copyright © 2020 Zapling Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
